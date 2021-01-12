2415a24d4aa689e21d3164b7194119efd61316ad
Marcelo <marcelo.home19@gmail.com>
2021-01-12 19:01:52 -0300

    Trocando o Type

--- src/main/java/br/com/pokedex/domain/enums/Type.java
+++ src/main/java/br/com/pokedex/domain/enums/Type.java
@@ -3,43 +3,23 @@ package br.com.pokedex.domain.enums;
 
 public enum Type {
 	
-	FIRE("Fogo"),
-	WATER("Água"),
-	EARTH("Terra"),
-	ELECTRIC("Elétrico"),
-	NORMAL("Normal"),
-	GRASS("Grama"),
-	ICE("Gelo"),
-	FIGHTER("Lutador"),
-	POISONOUS("Venenoso"),
-	FLYING("Voador"),
-	PSYCHIC("Psíquico"),
-	BUG("Inseto"),
-	STONE("Pedra"),
-	GHOST("Fantasma"),
-	DRAGON("Dragão"),
-	NIGHT("Noturno"),
-	FAIRY("Fada"),
-	STEEL("Aço");
-
-	private String description;
-	
-	private Type(String description) {
-		this.description=description;
-	}
-	
-	public  String getDescription() {
-		
-		return description;
-	}
-
-	public static String getDescription(String type){
-		for(Type types:Type.values()){
-			if(types.getDescription().equalsIgnoreCase(type)){
-				return types.getDescription();
-			}
-		}
-		return null;
-	}
+	FOGO,
+	AGUA,
+	TERA,
+	ELETRICO,
+	NORMAL,
+	GRAMA,
+	GELO,
+	LUTADOR,
+	VENENOSO,
+	VOADOR,
+	PSIQUICO,
+	INSETO,
+	PEDRA,
+	FANTASMA,
+	DRAGAO,
+	NOTURNO,
+	FADA,
+	ACO;
 
 }
